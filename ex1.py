17ec126ec42392713526fb5c4e68faad7ca06667
Zheng Jiang <jiangzheng916@163.com>
2016-07-25 15:48:54 +0800

    Create ex1.py

---
+++ ex1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 25 14:33:42 2016
+
+@author: Jiang
+"""
+score = 80 
+
+if score >= 90:  
+    print 'A'
+elif score >= 80:
+    print 'B'
+elif score >= 70:
+    print 'C'
+elif score >= 60:
+    print 'D'
+else: 
+    print 'E'
